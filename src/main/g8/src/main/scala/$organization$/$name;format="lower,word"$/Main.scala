9b5e0dc0b5b266676a90852ee5584c86cea503fd
Anton Parkhomenko <mailbox@chuwy.me>
2017-04-10 16:52:20 +0700

    Add basic scopt parser into Main class (close #3)

--- src/main/g8/src/main/scala/$organization$/$name;format="lower,word"$/Main.scala
+++ src/main/g8/src/main/scala/$organization$/$name;format="lower,word"$/Main.scala
@@ -13,8 +13,69 @@
 
 package $organization$.$name;format="lower,word"$
 
+import java.io.File
+
 object Main {
+
+  /**
+   * Raw CLI configuration used to extract options from command line
+   * Created solely for private `rawCliConfig` value and can contain
+   * incorrect state that should be handled by `transform` function
+   */
+  private case class CliConfig(value: Int, input: File, verbose: Boolean)
+
+  /**
+   * Starting raw value, required by `parser`
+   */
+  private val rawCliConfig = CliConfig(0, new File("."), false)
+
+  /**
+   * End application config parsed from CLI. Unlike `CliConfig`
+   */
+  case class AppConfig(value: Int, input: File, verbose: Boolean)
+
+
+  /**
+   * Check that raw config contains valid stat
+   */
+  def transform(raw: CliConfig): Either[String, AppConfig] = 
+    if (raw.value < 0) Left("Value cannot be less than 0")
+    else if (!raw.input.exists) Left(s"File [${raw.input.toString}] does not exist")
+    else Right(AppConfig(raw.value, raw.input, raw.verbose)
+
+  /**
+   * Scopt parser providing necessary argument annotations and basic validation
+   */
+  val parser = new scopt.OptionParser[Config](generated.ProjectMetadata.name) {
+    head(generated.ProjectMetadata.name, generated.ProjectMetadata.version)
+  
+    opt[Int]('v', "value").required().action( (x, c) =>
+      c.copy(value = x) ).text("Some value greater than zero")
+  
+    opt[File]('i', "input").required().valueName("<file>").
+      action( (x, c) => c.copy(input = x) ).
+      text("Input path")
+  
+    opt[Unit]("verbose").action( (_, c) =>
+      c.copy(verbose = true) ).text("Verbose output")
+  
+    help("help").text("prints this usage text")
+  
+    note("some notes.".newline)
+  }
+
+
   def main(args: Array[String]): Unit = {
     println("Hello $organization$.$name$!")
+
+    parser.parse(args, rawCliConfig).map(transform) match {
+      case Some(Right(appConfig)) => println(s"Success: $appConfig")
+      case Some(Left(error)) => 
+        // Failed transformation
+        println(error)
+        sys.exit(1)
+      case None =>
+        // Invalid arguments
+        sys.exit(1)
   }
 }
